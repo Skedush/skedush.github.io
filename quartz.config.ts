229ee28993d96f31aea769e0e1bddeae03cf7a8e
Skedush <candycakechocolate@hotmail.com>
2023-10-29 01:37:07 +0800

    Mac Pro vault backup: 2023-10-29 01:37:07
    Affected files:
    content/index.md
    quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Lii's Blog",
+    pageTitle: "💃 Lii's Blog",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "skedush.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
